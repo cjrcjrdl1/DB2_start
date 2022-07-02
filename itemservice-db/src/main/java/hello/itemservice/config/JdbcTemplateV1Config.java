e301f937cce0c39537ad40b1f362c21a207f390f
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-07-02 10:14:12 +0900

    JdbcTemplate 적용3 - 구성과 실행

---
+++ itemservice-db/src/main/java/hello/itemservice/config/JdbcTemplateV1Config.java
@@ -0,0 +1,33 @@
+package hello.itemservice.config;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV1;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+@Configuration
+@RequiredArgsConstructor
+public class JdbcTemplateV1Config {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JdbcTemplateItemRepositoryV1(dataSource);
+    }
+}
