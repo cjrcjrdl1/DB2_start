810665727fe791d2e0f1b5c51dae922b4e78b7bc
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-07-06 21:15:57 +0900

    스프링 데이터 JPA 적용2

--- itemservice-db/src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -1,9 +1,9 @@
 package hello.itemservice.repository.jpa;
 
 import hello.itemservice.domain.Item;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
